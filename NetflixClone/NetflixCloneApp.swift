dec78b4f8bef844d08788f42dfe9b65970a58f76
vanya <vanya.moo@gmail.com>
2024-10-16 16:50:20 +0100

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Vanya Mutafchieva on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
