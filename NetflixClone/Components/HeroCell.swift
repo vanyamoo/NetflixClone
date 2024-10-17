6489f8fe5d7dec4ab14ec5e479665ae70817742a
vanya <vanya.moo@gmail.com>
2024-10-17 16:57:25 +0100

    Add HeroCell

--- NetflixClone/Components/HeroCell.swift
+++ NetflixClone/Components/HeroCell.swift
@@ -6,13 +6,130 @@
 //
 
 import SwiftUI
+import SwiftfulUI
 
 struct HeroCell: View {
+    
+    var imageName = Constants.randomImage
+    var isNetflixFilm = true
+    var title = "Emily in Paris"
+    var genres = ["Raunchy", "Romantic", "Comedy"]
+    var onBackgroundPressed: (() -> Void)?
+    var onPlayPressed: (() -> Void)?
+    var onMyListPressed: (() -> Void)?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+            ImageLoaderView(urlString: imageName)
+            
+            VStack {
+                
+                netflixFilm
+                
+                filmTitle
+                
+                genresList
+                
+                HStack(spacing: 16) {
+                    play
+                    myList
+                }
+            }
+            .padding(32)
+            .background(LinearGradient(colors: [.netflixBlack.opacity(0.005), .netflixBlack.opacity(0.2), .netflixBlack.opacity(0.4)], startPoint: .top, endPoint: .bottom))
+        }
+        .cornerRadius(10)
+        .aspectRatio(0.8, contentMode: .fit)
+        .asButton(.tap) {
+            onBackgroundPressed?()
+        }
+    }
+    
+    private var netflixFilm: some View {
+        HStack(spacing: 8) {
+            Text("N")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.netflixRed)
+            
+            Text("FILM")
+                .kerning(3)
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .foregroundStyle(.netflixLightGray)
+        }
+        .opacity(isNetflixFilm ? 1 : 0)
+    }
+    
+    private var filmTitle: some View {
+        Text(title)
+            .font(.system(size: 45, weight: .medium, design: .serif))
+            .multilineTextAlignment(.center)
+            .lineLimit(3)
+            .foregroundStyle(.netflixWhite)
+            .shadow(radius: 4)
+    }
+    
+    private var genresList: some View {
+        HStack {
+            ForEach(genres, id:\.self) { genre in
+                HStack {
+                    Text(genre)
+                        .font(.callout)
+                        .fontWeight(.medium)
+                        .lineLimit(3)
+                        
+                    
+                    Circle()
+                        .frame(width: 5)
+                        .opacity(genres.last == genre ? 0 : 1)
+                }
+                .foregroundStyle(.netflixWhite)
+                .shadow(radius: 4)
+            }
+        }
+    }
+    
+    private var play: some View {
+        RoundedRectangle(cornerRadius: 4)
+            .fill(.netflixWhite)
+            .overlay {
+                HStack(spacing: 4) {
+                    Image(systemName: "play.fill")
+                    Text("Play")
+                }
+                .foregroundStyle(.netflixGray)
+                .fontWeight(.semibold)
+            }
+            .frame(width: 130, height: 36)
+            .asButton {
+                onPlayPressed?()
+            }
+    }
+    
+    private var myList: some View {
+        RoundedRectangle(cornerRadius: 4)
+            .fill(.netflixGray)
+            .overlay {
+                HStack(spacing: 4) {
+                    Image(systemName: "plus")
+                    Text("My List")
+                }
+                .foregroundStyle(.netflixWhite)
+                .fontWeight(.semibold)
+            }
+            .frame(width: 130, height: 36)
+            .asButton {
+                onMyListPressed?()
+            }
     }
 }
 
 #Preview {
-    HeroCell()
+    ZStack {
+        Color.netflixBlack.ignoresSafeArea()
+        
+        HeroCell()
+            .padding(40)
+    }
 }
